4b261b4075bb0d13ea1ef078a12332bb919e01ac
morarodgers <morarodgers@gmail.com>
2023-12-19 07:54:38 +0300

    Corrections for week 10

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,7 +10,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if(err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors)
     .map((item) => item.message)
-    .json(',')
+    .join(',')
     customError.statusCode = 400
   }
 
