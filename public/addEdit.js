0ba0dba22974ac382d19db15494b844be3faaa79
morarodgers <morarodgers@gmail.com>
2024-01-12 00:43:51 +0300

    Assignment for week 12

---
+++ public/addEdit.js
@@ -0,0 +1,157 @@
+import { enableInput, inputEnabled, message, setDiv, token } from "./index.js";
+import { showNotes } from "./notes.js";
+
+let addEditDiv = null;
+let title = null;
+let content = null;
+let subject = null;
+let addingNote = null;
+let deletingNote = null;
+
+export const handleAddEdit = () => {
+ // let addEditDiv, title, subject, content, addingNote, deletingNote, token;
+
+  addEditDiv = document.getElementById("edit-note");
+  title = document.getElementById("title");
+  subject = document.getElementById("subject");
+  content = document.getElementById("content");
+  addingNote = document.getElementById("adding-note");
+  deletingNote = document.getElementById("deleting-note");
+  const editCancel = document.getElementById("edit-cancel");
+
+  addEditDiv.addEventListener("click", async (e) => {
+    if (inputEnabled && e.target.nodeName === "BUTTON") {
+      if (e.target === addingNote) {
+        enableInput(false);
+
+        let method = "POST";
+        let url = "/api/v1/notes";
+
+        // Edit a note
+        if (addingNote.textContent === "update") {
+          method = "PATCH";
+          url = `/api/v1/notes/${addEditDiv.dataset.id}`;
+        }
+
+        try {
+          const response = await fetch(url, {
+            method: method,
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              title: title.value,
+              subject: subject.value,
+              content: content.value,
+            }),
+          });
+
+          const data = await response.json();
+          if (response.status === 200 || response.status === 201) {
+            if (response.status === 200) {
+              // a 200 is expected for a successful update
+              message.textContent = "The note entry was updated.";
+            } else {
+              // a 201 is expected for a successful creation of a note
+              message.textContent = "The note entry was created.";
+            }
+
+            title.value = "";
+            subject.value = "";
+            content.value = "";
+
+            showNotes();
+          } else {
+            message.textContent = data.msg;
+          }
+        } catch (err) {
+          console.log(err);
+          message.textContent = "A communication error occurred.";
+        }
+
+        enableInput(true);
+
+      } else if (e.target === editCancel) {
+        message.textContent = "";
+        
+        showNotes();
+
+      } else if (e.target.classList.contains("deleteButton")) {
+        // Get the noteId from the data attribute
+        const noteId = e.target.dataset.id;
+
+        try {
+          const response = await fetch(`/api/v1/notes/${noteId}`, {
+            method: "DELETE",
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          });
+
+          if (response.status === 204) {
+            console.log('Note deleted successfully');
+            
+            showNotes();
+
+          } else {
+            // Handle error
+            const data = await response.json();
+            console.error('Error deleting Note:', data.msg);
+          }
+        } catch (err) {
+          console.error('A communication error occurred:', err);
+        }
+
+        showNotes();
+      }
+    }
+  });
+};
+
+export const showAddEdit = async (noteId) => {
+  if (!noteId) {
+    title.value = "";
+    subject.value = "";
+    content.value = "";
+    addingNote.textContent = "add";
+    message.textContent = "";
+
+    setDiv(addEditDiv);
+  } else {
+    enableInput(false);
+
+    try {
+      const response = await fetch(`/api/v1/notes/${noteId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      if (response.status === 200) {
+        title.value = data.note.title;
+        subject.value = data.note.subject;
+        content.value = data.note.content;
+        addingNote.textContent = "update";
+        message.textContent = "";
+        addEditDiv.dataset.id = noteId;
+
+        setDiv(addEditDiv);
+      } else {
+        // might happen if the list has been updated since last display
+        message.textContent = "The note entry was not found";
+        showNotes();
+      }
+    } catch (err) {
+      console.log(err);
+      message.textContent = "A communications error has occurred.";
+      showNotes();
+    }
+
+    enableInput(true);
+  }
+};
---
+++ public/notes.js
@@ -0,0 +1,88 @@
+import {
+    inputEnabled,
+    setDiv,
+    message,
+    setToken,
+    token,
+    enableInput,
+  } from "./index.js";
+  import { showLoginRegister } from "./loginRegister.js";
+  import { showAddEdit } from "./addEdit.js";
+  
+  let notesDiv = null;
+  let notesTable = null;
+  let notesTableHeader = null;
+  
+  export const handleNotes = () => {
+    notesDiv = document.getElementById("notes");
+    const logoff = document.getElementById("logoff");
+    const addNote = document.getElementById("add-note");
+    notesTable = document.getElementById("notes-table");
+    notesTableHeader = document.getElementById("notes-table-header");
+  
+    notesDiv.addEventListener("click", (e) => {
+      if (inputEnabled && e.target.nodeName === "BUTTON") {
+        if (e.target === addNote) {
+          showAddEdit(null);
+        } else if (e.target === logoff) {
+            setToken(null);
+            message.textContent = "You have been logged off.";
+            notesTable.replaceChildren([notesTableHeader]);
+
+            showLoginRegister();
+        } else if (e.target.classList.contains("editButton")) {
+            message.textContent = "";
+            showAddEdit(e.target.dataset.id);
+        } else if (e.target.classList.contains("deleteButton")) {
+          message.textContent = "";
+          showAddEdit(e.target.dataset.id);
+      }
+      }
+    });
+  };
+  
+  export const showNotes = async () => {
+    try {
+        enableInput(false);
+    
+        const response = await fetch("/api/v1/notes", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+    
+        const data = await response.json();
+        let children = [notesTableHeader];
+    
+        if (response.status === 200) {
+          if (data.count === 0) {
+            notesTable.replaceChildren(...children); // clear this for safety
+          } else {
+            for (let i = 0; i < data.notes.length; i++) {
+              let rowEntry = document.createElement("tr");
+    
+              let editButton = `<td><button type="button" class="editButton" data-id=${data.notes[i]._id}>edit</button></td>`;
+              let deleteButton = `<td><button type="button" class="deleteButton" data-id=${data.notes[i]._id}>delete</button></td>`;
+              let rowHTML = `
+                <td>${data.notes[i].title}</td>
+                <td>${data.notes[i].subject}</td>
+                <td>${data.notes[i].content}</td>
+                <div>${editButton}${deleteButton}</div>`;
+    
+              rowEntry.innerHTML = rowHTML;
+              children.push(rowEntry);
+            }
+            notesTable.replaceChildren(...children);
+          }
+        } else {
+          message.textContent = data.msg;
+        }
+      } catch (err) {
+        console.log(err);
+        message.textContent = "A communication error occurred.";
+      }
+      enableInput(true);
+      setDiv(notesDiv);
+    };
